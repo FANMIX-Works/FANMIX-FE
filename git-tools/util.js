55ab5fc1d122eaab28c9d6d55041f0b5bd3485e0
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-02 02:56:52 +0900

    📦 chore: dividerLength 제거, divider가 message 길이에 따라 유동적이게 수정 (#2)

--- git-tools/util.js
+++ git-tools/util.js
@@ -2,11 +2,10 @@
  * 주어진 메시지를 장식하여 콘솔에 출력합니다.
  * @param {string} message - 출력할 메시지
  * @param {string} [dividerChar='='] - 구분선에 사용할 문자
- * @param {number} [dividerLength=50] - 구분선의 길이
  * @returns {string} decoratedMessage - 구분선과 합쳐진 메시지
  */
-function showDecoratedMessage(message, dividerChar = '=', dividerLength = 50) {
-  const divider = dividerChar.repeat(dividerLength);
+function showDecoratedMessage(message, dividerChar = '=') {
+  const divider = dividerChar.repeat(message.length + 20);
   const decoratedMessage = `
       ${divider}
       ${message}
