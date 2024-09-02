3486ee2500679aa3e6293045c8b3f916f44039a0
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-02 18:44:10 +0900

    📦 chore: 파일 권한 부여 경로 수정, 직접 실행해도 돌아가게 npx 추가 (#2)

--- scripts/commitizen.sh
+++ scripts/commitizen.sh
@@ -13,7 +13,7 @@ fi
 # pre-commit 훅이 성공적으로 실행되었다면 Commitizen 실행 (commit-msg 체크 건너 뜀)
 # 직접 git commit 하는 경우가 있으니, 그때도 체크가 필요하기에 commit-msg 유지 필요
 if [ $? -eq 0 ]; then
-  HUSKY=0 cz
+  HUSKY=0 npx cz
 else
   echo "Pre-commit hook failed. Aborting commit."
   exit 1
--- scripts/update_permissions.sh
+++ scripts/update_permissions.sh
@@ -4,7 +4,7 @@
 chmod +x .husky/pre-commit
 chmod +x .husky/commit-msg
 
-# scripts 폴더 내 쉘 스크립트에 실행 권한 부여
-chmod +x scripts/*.sh
+# scripts 폴더 내 commitizen 스크립트에 실행 권한 부여
+chmod +x ./scripts/commitizen.sh
 
 echo "File permissions updated successfully."
