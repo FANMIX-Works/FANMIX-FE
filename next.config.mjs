4bd1dd15de2ab86b14c41497cadc1a8da8cf1d6d
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-06 02:59:56 +0900

    ✨ feat: @ducanh2912/next-pwa 라이브러리 추가, pwa 기본 manifest 세팅 (#12)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,57 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { default as withPWA } from '@ducanh2912/next-pwa';
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default pwaConfig(nextConfig);
