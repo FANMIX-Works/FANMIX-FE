f08274d6021fee05f73476e3695835ad6d59d963
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-06 14:12:17 +0900

    ✨ feat: manifest.json 수정, themeColor 설정 (#12)

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,9 @@ import { default as withPWA } from '@ducanh2912/next-pwa';
 
 const pwaConfig = withPWA({
   dest: 'public',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  workboxOptions: {
-    disableDevLogs: true,
-  },
 });
 
 /** @type {import('next').NextConfig} */
