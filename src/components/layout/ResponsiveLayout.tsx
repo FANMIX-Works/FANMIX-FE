9401495515f49b6bde1f4adec168451658222a5d
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-11 18:59:25 +0900

    ✨ feat: 배경 스크롤 제한,전체 높이는 동적 높이로 변경(모바일 최적화) (#18)

--- src/components/layout/ResponsiveLayout.tsx
+++ src/components/layout/ResponsiveLayout.tsx
@@ -4,8 +4,8 @@ interface ResponsiveLayoutProps {
 
 const ResponsiveLayout = ({ children }: ResponsiveLayoutProps) => {
   return (
-    <div className="h-screen w-screen bg-slate-900 flex-center">
-      <div className="relative h-screen max-h-[900px] w-screen overflow-hidden rounded-md border border-slate-600 bg-black text-white flex-col-center sm:w-[393px]">
+    <div className="h-dvh w-screen overflow-hidden bg-slate-900 flex-center">
+      <div className="relative h-full max-h-[900px] w-full overflow-hidden bg-black text-white flex-col-center sm:w-[393px]">
         {children}
       </div>
     </div>
