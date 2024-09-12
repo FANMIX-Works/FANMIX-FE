b39b411e88ab75c2ea9849afa601116d338dfd0d
dandanii22 <dandanii0816@gmail.com>
2024-09-12 21:02:42 +0900

    ✨ feat: 로그인 페이지 마크업, CSS (#15)

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -2,10 +2,11 @@
 
 import { useRouter } from '@/i18n/routing';
 import { RiCloseLargeLine } from 'react-icons/ri';
-import LogoImg from '@/assets/logo/logo_login.svg';
+import LogoImg from '@/assets/logo/logo_vertical.svg';
 import { FcGoogle } from 'react-icons/fc';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Button } from '@/components/ui/button';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -21,18 +22,18 @@ export default function LoginPage() {
   return (
     <main className="h-full w-full bg-gradient">
       <div>
-        <p className="flex items-center justify-end">
-          <RiCloseLargeLine className="closeIcon" />
+        <p className="flex items-center justify-end p-3 pb-0">
+          <RiCloseLargeLine className="mt-[10px] h-6 w-6 cursor-pointer" />
         </p>
         <div>
           <Swiper
-            className="mt-10 imgArea"
+            className="mt-[58px] h-[450px] w-[394px]"
             modules={[Pagination]}
             slidesPerView={1}
             pagination={{ clickable: true }}>
-            <SwiperSlide className="flex h-full items-center justify-center">
+            <SwiperSlide className="flex items-center justify-center">
               <div className="flex h-full w-full items-center justify-center">
-                <LogoImg className="max-h-full max-w-full" />
+                <LogoImg className="mb-[119px] mt-[120px] max-h-full max-w-full" />
               </div>
             </SwiperSlide>
             <SwiperSlide></SwiperSlide>
@@ -41,11 +42,11 @@ export default function LoginPage() {
           </Swiper>
         </div>
       </div>
-      <div className="flex-col-center">
-        <button className="flex items-center justify-center loginBtn hover:hover-loginBtn">
+      <div className="flex flex-col items-center">
+        <Button className="mb-[25px] mt-[50px] flex h-[52px] w-[353px] items-center justify-center bg-[#262626] hover:bg-[#171717]">
           <FcGoogle className="mr-4 h-5 w-5" />
-          <span className="text-black-54 font-semibold">Google 계정으로 시작하기</span>
-        </button>
+          <span className="button-r">Google 계정으로 시작하기</span>
+        </Button>
         <button onClick={handleGoBack}>뒤로가기</button>
       </div>
     </main>
