89876879a51ebbc1904c38000087abd7942eedda
dandanii22 <dandanii0816@gmail.com>
2024-09-13 03:18:50 +0900

    ✨ feat: tailwindCSS config 파일 변경, 마크업 수정 (#15)

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { useRouter } from '@/i18n/routing';
-import { RiCloseLargeLine } from 'react-icons/ri';
+import { VscChromeClose } from 'react-icons/vsc';
 import LogoImg from '@/assets/logo/logo_vertical.svg';
 import { FcGoogle } from 'react-icons/fc';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -19,30 +17,27 @@ export default function LoginPage() {
   };
 
   return (
-    <main className="h-full w-full bg-gradient">
-      <div>
-        <p className="flex items-center justify-end p-3 pb-0">
-          <RiCloseLargeLine className="mt-[10px] h-6 w-6 cursor-pointer" />
-        </p>
-        <div>
-          <Swiper
-            className="mt-[58px] h-[450px] w-[394px]"
-            modules={[Pagination]}
-            slidesPerView={1}
-            pagination={{ clickable: true }}>
-            <SwiperSlide className="flex items-center justify-center">
-              <div className="flex h-full w-full items-center justify-center">
-                <LogoImg className="mb-[119px] mt-[120px] max-h-full max-w-full" />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-          </Swiper>
-        </div>
+    <main className="h-full w-full dark-gradient">
+      <div className="flex items-center justify-end p-3 pb-0">
+        <VscChromeClose className="mt-[10px] h-6 w-6 scale-transition-105" onClick={handleGoBack} />
       </div>
-      <div className="flex flex-col items-center">
-        <button className="mb-[25px] mt-[50px] flex h-[52px] w-[353px] items-center justify-center bg-[#262626] hover:bg-[#171717]">
+      <Swiper
+        className="mt-[58px] h-[450px] w-[394px]"
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+        <SwiperSlide className="flex items-center justify-center">
+          <div className="flex items-center justify-center">
+            <LogoImg className="mb-[119px] mt-[167px] max-h-full max-w-full" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+      </Swiper>
+
+      <div className="mx-5 flex flex-col items-center">
+        <button className="mb-[24px] mt-[50px] flex h-[52px] w-full items-center justify-center bg-neutral-800 hover:bg-neutral-900">
           <FcGoogle className="mr-4 h-5 w-5" />
           <span className="button-r">Google 계정으로 시작하기</span>
         </button>
