d63e2a23d812078c054a4fb3ba1e187301f71a32
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-13 17:18:33 +0900

    ✨ feat: 스크롤 방향에 따른 하단 네비게이션 visible 처리 추가 (#19)

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -6,9 +6,12 @@ import { useTranslations } from 'next-intl';
 export default function MainPage() {
   const t = useTranslations('loginPage');
   const t2 = useTranslations('mainPage');
+
   // #20240908.syjang, 아래 내용은 다국어 처리 및 언어 전환 예시로 작성해뒀습니다. 다음 pr 때 삭제 예정입니다.
   return (
-    <main className="h-full w-full overflow-y-auto px-5 scrollbar-hide-smooth">
+    <main
+      id="current-scroll-page"
+      className="h-full w-full overflow-y-auto px-5 scrollbar-hide-smooth">
       메이인 메이인 메이인 메이인 메이인 메이인 메이인 메이인 메이인
       <h1>{t('나중에 하기')}</h1>
       <h1>{t2('{nickName}님 안녕하세요', { nickName: 'tpdud' })}</h1>
