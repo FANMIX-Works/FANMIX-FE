ef4845446a3885c0bd7edc5875a03dd8525596d7
jangseyeong <jang.se.yeong000@gmail.com>
2024-10-01 04:50:59 +0900

    ✨ feat: ButtonListDrawer 컴포넌트 추가 (#67)

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -40,11 +40,11 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background',
+        'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-neutral-600 bg-neutral-900/90',
         className,
       )}
       {...props}>
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-neutral-700" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
