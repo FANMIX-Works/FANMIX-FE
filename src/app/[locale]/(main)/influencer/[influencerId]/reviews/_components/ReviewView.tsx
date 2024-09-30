7fddf376fb12b33b1521355b957cf7003ddcde68
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-30 21:23:15 +0900

    ✨ feat: 15일 경과 ui 추가 (#55)

--- src/app/[locale]/(main)/influencer/[influencerId]/reviews/_components/ReviewView.tsx
+++ src/app/[locale]/(main)/influencer/[influencerId]/reviews/_components/ReviewView.tsx
@@ -11,7 +11,9 @@ type ReviewViewProps = {
 };
 const ReviewView = ({ setReviewMode }: ReviewViewProps) => {
   const t = useTranslations('review_view');
+
   const testData = {
+    isWithin15Days: true,
     contentsRating: 3,
     communicationRating: 4,
     trustRating: 4,
@@ -24,6 +26,9 @@ const ReviewView = ({ setReviewMode }: ReviewViewProps) => {
   const handleEditReview = () => {
     setReviewMode(REVIEW_MODE.EDIT);
   };
+  const handlePostReview = () => {
+    setReviewMode(REVIEW_MODE.CREATE);
+  };
 
   return (
     <div className="flex flex-col justify-center gap-2.5">
@@ -34,18 +39,35 @@ const ReviewView = ({ setReviewMode }: ReviewViewProps) => {
         </div>
         <MetricsText {...testData} />
       </div>
-      <div className="min-h-[76px] w-full bg-neutral-800 p-2 body3-r">
+      <p className="min-h-[76px] w-full bg-neutral-800 p-2 body3-r">
         내 글 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 내 글 텍스트 텍스트 텍스트 텍스트 트
         내 글 텍스트 텍스트 텍스트 텍스트
-      </div>
-      <div className="w-full gap-2 flex-center">
-        <Button variant="outline" className="h-[34px] flex-1 body3-r" onClick={handleDeleteReview}>
-          {t('삭제하기')}
-        </Button>
-        <Button variant="white" className="h-[34px] flex-1 body3-m" onClick={handleEditReview}>
-          {t('수정하기')}
-        </Button>
-      </div>
+      </p>
+      {testData.isWithin15Days ? (
+        <div className="flex-center">
+          <span className="flex-1 whitespace-break-spaces text-orange-500 sub1-r">
+            {t('리뷰 작성 후 15일이 경과하여 수정, 삭제가 불가합니다')}
+          </span>
+          <Button
+            variant="outline"
+            className="h-[34px] w-[150px] flex-shrink-0 body3-m"
+            onClick={handlePostReview}>
+            리뷰 새로 남기기
+          </Button>
+        </div>
+      ) : (
+        <div className="w-full gap-2 flex-center">
+          <Button
+            variant="outline"
+            className="h-[34px] flex-1 body3-r"
+            onClick={handleDeleteReview}>
+            {t('삭제하기')}
+          </Button>
+          <Button variant="white" className="h-[34px] flex-1 body3-m" onClick={handleEditReview}>
+            {t('수정하기')}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
