29594e47e33afc26aed051749fb527de9277b7d5
jangseyeong <jang.se.yeong000@gmail.com>
2024-10-03 03:30:26 +0900

    ✨ feat: 콘텐츠 지향성 다국어 추가 (#80)

--- src/app/[locale]/(main)/influencer/[influencerId]/_components/ContentTraits.tsx
+++ src/app/[locale]/(main)/influencer/[influencerId]/_components/ContentTraits.tsx
@@ -1,20 +1,17 @@
 import { useMemo } from 'react';
+import { useTranslations } from 'next-intl';
 
 import { Separator } from '@/components/ui/separator';
 import { ENERGY, ORIGINALITY, TONE } from '@/constants/contentTraits';
 
-// #20240929.syjang, 컴포넌트 정리 필요
 interface ContentTraitsProps {
   originalityScore: number; // 대중적-독창적
   toneScore: number; // 가벼운-진지한
   energyScore: number; // 차분한-역동적
 }
 
-const ContentTraits = ({
-  originalityScore = 5,
-  toneScore = 5,
-  energyScore = 5,
-}: ContentTraitsProps) => {
+const ContentTraits = ({ originalityScore, toneScore, energyScore }: ContentTraitsProps) => {
+  const t = useTranslations('content_trait');
   const traitList = useMemo(
     () => [ORIGINALITY[originalityScore - 1], TONE[toneScore - 1], ENERGY[energyScore - 1]],
     [originalityScore, toneScore, energyScore],
@@ -26,7 +23,7 @@ const ContentTraits = ({
         <li key={LABEL} className="flex flex-1 items-center">
           <div className="flex-1 gap-0.5 text-lime-400 flex-col-center">
             <ICON className="h-[22px] w-[22px]" />
-            <div className="text-white body2-m">{LABEL}</div>
+            <div className="text-white body3-m">{t(LABEL)}</div>
           </div>
           {index !== traitList.length - 1 && (
             <Separator
--- src/constants/contentTraits.ts
+++ src/constants/contentTraits.ts
@@ -8,21 +8,22 @@ import {
   LiaFeatherAltSolid,
 } from 'react-icons/lia';
 
+// LABEL == 다국어 키값
 export const ORIGINALITY = [
   { LABEL: '매우 대중적', ICON: LiaEllo },
   { LABEL: '대중적', ICON: LiaEllo },
   { LABEL: '보편적', ICON: LiaHashtagSolid },
   { LABEL: '독창적', ICON: LiaJira },
   { LABEL: '매우 독창적', ICON: LiaJira },
-];
+] as const;
 
 export const TONE = [
-  { LABEL: '매우 가벼움', ICON: LiaFeatherAltSolid },
-  { LABEL: '가벼움', ICON: LiaFeatherAltSolid },
+  { LABEL: '매우 가벼운', ICON: LiaFeatherAltSolid },
+  { LABEL: '가벼운', ICON: LiaFeatherAltSolid },
   { LABEL: '균형적', ICON: LiaHashtagSolid },
   { LABEL: '진지한', ICON: LiaDatabaseSolid },
   { LABEL: '매우 진지한', ICON: LiaDatabaseSolid },
-];
+] as const;
 
 export const ENERGY = [
   { LABEL: '매우 차분한', ICON: LiaWaterSolid },
@@ -30,4 +31,4 @@ export const ENERGY = [
   { LABEL: '조화로운', ICON: LiaHashtagSolid },
   { LABEL: '역동적', ICON: LiaStreamSolid },
   { LABEL: '매우 역동적', ICON: LiaStreamSolid },
-];
+] as const;
