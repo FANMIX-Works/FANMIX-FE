0384fb87a6ead9de59db5df970663721822fda22
jangseyeong <jang.se.yeong000@gmail.com>
2024-10-04 00:19:31 +0900

    ✨ feat: 서비스 형태 객체로 변경, axios 요청/응답 인터셉터 추가 (#28)

--- src/hooks/queries/useAuthService.ts
+++ src/hooks/queries/useAuthService.ts
@@ -4,26 +4,26 @@ import { useMutation } from '@tanstack/react-query';
 import { useAuthStore } from '@/stores/authStore';
 import { useInformationToast } from '../useInformationToast';
 
-import { logout } from '@/services/authService';
+import { authService } from '@/services/authService';
 
 export const useLogout = () => {
   const setLogout = useAuthStore((state) => state.setLogout);
   const { showConfirmToast, showErrorToast } = useInformationToast();
 
   return useMutation<AxiosError>({
-    mutationFn: logout,
+    mutationFn: authService.logout,
     onSuccess: () => {
       setLogout();
       showConfirmToast('로그아웃 되었습니다.');
 
-      // 로그아웃 시 홈 페이지로 리다이렉트
-      window.location.href = '/';
+      // // 로그아웃 시 홈 페이지로 리다이렉트
+      // window.location.href = '/';
 
-      // 뒤로가기 방지
-      window.history.pushState(null, '', window.location.href);
-      window.onpopstate = function () {
-        window.history.pushState(null, '', window.location.href);
-      };
+      // // 뒤로가기 방지
+      // window.history.pushState(null, '', window.location.href);
+      // window.onpopstate = function () {
+      //   window.history.pushState(null, '', window.location.href);
+      // };
     },
     onError: (error) => {
       console.error('로그아웃 실패:', error);
