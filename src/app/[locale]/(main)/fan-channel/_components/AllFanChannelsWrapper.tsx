d10d947de5244e1eb35da586af554fb7a62385b4
jangseyeong <jang.se.yeong000@gmail.com>
2024-10-15 12:24:37 +0900

    📦 chore: 팬채널 리스트에 필요없는 스피너 제거 (#94)

--- src/app/[locale]/(main)/fan-channel/_components/AllFanChannelsWrapper.tsx
+++ src/app/[locale]/(main)/fan-channel/_components/AllFanChannelsWrapper.tsx
@@ -4,12 +4,10 @@ import { useAllFanChannelList } from '../_hooks/useAllFanChannelList';
 
 import FanChannelList from './FanChannelList';
 import SortOptionsList from '@/components/common/SortOptionsList';
-import ComponentSpinner from '@/components/common/spinner/ComponentSpinner';
 
 const AllFanChannelsWrapper = () => {
   const { fanChannelListData, isLoading, isError, sortButtons } = useAllFanChannelList();
 
-  if (isLoading) return <ComponentSpinner className="h-full flex-center" />;
   return (
     <section aria-label="인증된 인플루언서들의 팬채널 리스트" className="h-full">
       <SortOptionsList className="mb-[15px]" sortButtons={sortButtons} />
