1ad9e40cc4133daaa7c82e8509a37f8f2a2a4f66
jangseyeong <jang.se.yeong000@gmail.com>
2024-10-15 16:18:50 +0900

    ✨ feat: 팬채널리스트 페이지에 defaultSort 지정 추가 (#94)

--- src/app/[locale]/(main)/fan-channel/_components/AllFanChannelsWrapper.tsx
+++ src/app/[locale]/(main)/fan-channel/_components/AllFanChannelsWrapper.tsx
@@ -5,8 +5,11 @@ import { useAllFanChannelList } from '../_hooks/useAllFanChannelList';
 import FanChannelList from './FanChannelList';
 import SortOptionsList from '@/components/common/SortOptionsList';
 
-const AllFanChannelsWrapper = () => {
-  const { fanChannelListData, isLoading, isError, sortButtons } = useAllFanChannelList();
+interface AllFanChannelsWrapperProps {
+  defaultSort?: string;
+}
+const AllFanChannelsWrapper = ({ defaultSort }: AllFanChannelsWrapperProps) => {
+  const { fanChannelListData, isLoading, isError, sortButtons } = useAllFanChannelList(defaultSort);
 
   return (
     <section aria-label="인증된 인플루언서들의 팬채널 리스트" className="h-full">
