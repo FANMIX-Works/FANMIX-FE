a9e864fda224621c17ebecf050d3af206151f6f4
jangseyeong <jang.se.yeong000@gmail.com>
2024-11-08 22:18:16 +0900

    🛠️ refactor: UserAvatar, Like/DislikeButton UI 공통으로 분리 (#139)

--- src/components/domain/user/UserAvatar.tsx
+++ src/components/domain/user/UserAvatar.tsx
@@ -13,7 +13,7 @@ const UserAvatar = ({ size, userNickName, profileImgUrl }: UserAvatarProps) => {
   return (
     <div className="relative flex-shrink-0" style={{ width: `${size}px`, height: `${size}px` }}>
       <Avatar className="h-full w-full flex-shrink-0">
-        <AvatarImage src={profileImgUrl} alt="유저 프로필 사진" />
+        <AvatarImage src={profileImgUrl} alt={`유저 ${userNickName}의 프로필 사진`} />
         <AvatarFallback className="bg-orange-300/40 h1-sb">{userNickName[0]}</AvatarFallback>
       </Avatar>
     </div>
